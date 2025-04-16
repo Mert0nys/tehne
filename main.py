4a4abbd4a04f6d4bfabe3660277886ee74ade363
Mert0nys <makarovm2007@gmail.com>
2025-04-16 18:19:27 +0400

    tehne1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from telebot import TeleBot
 import pandas as pd
 from database import insert
 from parser import parse_price
+from io import BytesIO
 
 BOT_TOKEN = "7948451831:AAH5zxEcHCaM3hDZZ_X1ogvf3GXWREnyZb0"
 bot = TeleBot(BOT_TOKEN)
@@ -15,26 +16,34 @@ def handle_document(message):
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
-    with open('data.xlsx', 'wb') as new_file:
-        new_file.write(downloaded_file)
+    excel_file = BytesIO(downloaded_file)
+    
+    try:
+        df = pd.read_excel(excel_file)
 
-    df = pd.read_excel('data.xlsx')
-    bot.send_message(message.chat.id, f"Загруженные данные:\n{df.to_string()}")
+        required_columns = ['url', 'xpath', 'title']
+        if not all(column in df.columns for column in required_columns):
+            bot.send_message(message.chat.id, "Ошибка: отсутствуют необходимые столбцы в загруженном файле.")
+            return
 
-    required_columns = ['url', 'xpath', 'title']
-    if not all(column in df.columns for column in required_columns):
-        bot.send_message(message.chat.id, "Ошибка: отсутствуют необходимые столбцы в загруженном файле.")
-        return
+        # Получаем последнюю строку
+        last_row = df.iloc[-1]
+        last_row_message = f"*Последнее добавление:*\n\n*Title:* {last_row['title']}\n*URL:* {last_row['url']}\n*XPath:* {last_row['xpath']}"
 
-    for index, row in df.iterrows():
+        bot.send_message(message.chat.id, last_row_message, parse_mode='Markdown')
+
+        # Обработка последней строки для получения цены и вставки в базу данных
         try:
-            price = parse_price(row['url'], row['xpath'])
+            price = parse_price(last_row['url'], last_row['xpath'])
             if price is not None: 
-                insert((row['title'], row['url'], row['xpath'], price))  
+                insert((last_row['title'], last_row['url'], last_row['xpath'], price))  
             else:
-                bot.send_message(message.chat.id, f"Не удалось получить цену для строки {index + 1}.")
+                bot.send_message(message.chat.id, "Не удалось получить цену для последнего добавления.")
         except Exception as e:
-            bot.send_message(message.chat.id, f"Ошибка при обработке строки {index + 1}: {str(e)}")
+            bot.send_message(message.chat.id, f"Ошибка при обработке последней строки: {str(e)}")
+
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Ошибка при чтении файла: {str(e)}")
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
